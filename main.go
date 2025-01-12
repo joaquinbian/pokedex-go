5c9baaa562f7557945639fb9300d0e27310aebca
joaquinbian <joacobian21@gmail.com>
2025-01-12 15:51:42 -0300

    repl

--- main.go
+++ main.go
@@ -1,14 +1,6 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
 
-	fmt.Print("Hello, World!")
-	v := cleanInput("    ")
-	a := []string{}
-	fmt.Print(v, len(v))
-	fmt.Print(a, len(a))
+	startRepl()
 }
--- repl.go
+++ repl.go
@@ -1,8 +1,27 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+func startRepl() {
+	for {
+		scanner := bufio.NewScanner(os.Stdin)
+		fmt.Print("Pokedex > ")
+		if scanner.Scan() {
+			text := cleanInput(scanner.Text())
+			if len(text) > 0 {
+
+				fmt.Printf("Your command was: %v\n", text[0])
+			}
+
+		}
+
+	}
+}
 
 func cleanInput(text string) []string {
 	textTrimed := strings.Trim(text, " ")
